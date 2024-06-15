6aea74ee0a54626bc45fc99e2513d6f8ef6eef0e
Eduard Akhmadullin <EAkhmadullin@slb.com>
2024-06-15 22:06:28 +0500

    PascalTriangle commit

---
+++ pascalTriangle.py
@@ -0,0 +1,12 @@
+N = int(input())
+T = []
+for i in range (N):
+    row = [1] * (i+1)
+    for j in range (i+1):
+        if j != 0 and j != i:
+            row[j] = T[i-1][j-1]+T[i-1][j]
+    T.append(row)            
+
+for k in T:
+    print (*k)
+
--- test5_6.py
+++ test5_6.py
@@ -5,19 +5,4 @@ for i in range(n):
     for j in range(0,n-1-i):
         if lst[j]>lst[j+1]:
             lst[j],lst[j+1]=lst[j+1],lst[j]
-            lstj = lst[j]
-            lsti = lst[i]
-            max = lst[j]
-
-
-
-
-
-
-for i in range(0, n):
-    for j in range(i,n-1-i):
-        if lst[j]>lst[j+1]:
-            lst[j],lst[j+1]=lst[j+1],lst[j]
-            lstj = lst[j]
-            lsti = lst[i]
-            max = lst[j]
+print (*lst)
